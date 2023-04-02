c84a39767b23e9433f7e19e7d5131e5ef49e5601
48lexR <76016929+48lexR@users.noreply.github.com>
2023-04-02 13:25:37 -0400

    Revamped due to timing errors with other method.

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from main import Lexobot
-from main import API_KEY, HOST, USERNAME
-
-__version__ = "1.0.0"
-
-lexobot = Lexobot(host=HOST, api_key=API_KEY, username=USERNAME)
-
-if __name__ == "__main__":
-    lexobot()
--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 from pydiscourse import DiscourseClient
-from extra_functions import getNotifications, htmlStripper, getPostReplyContent, getTopicReplyNumber, getPostReplyNumber
+from bs4 import BeautifulSoup
+import re
 import time
-HOST = 'HOST'
-API_KEY = 'APIKEY'
+HOST = 'https://forum.wackywriters.com/'
+API_KEY = 'c693a7de660e6855ef397e0c0cc25b9c9fa5974287e5196bdfaa41ba079a6cfc'
 USERNAME = 'Lexobot'
+
+
+def htmlStripper(text: str):
+    try:
+        soup = BeautifulSoup(text, 'html.parser')
+    except TypeError:
+        return
+    soup = soup.get_text()
+    new_text = re.sub('@', '', soup)
+    return new_text
+
+
+def getNotifications(client: DiscourseClient):
+    return client._get('/notifications.json')['notifications']
+
+
+def getTopicReplyNumber(client: DiscourseClient):
+    return client._get('/notifications.json')['notifications'][0]['topic_id']
+
+
+def getPostReplyNumber(client: DiscourseClient):
+    return client._get('/notifications.json')['notifications'][0]['post_number']
+
+
+def getUserReply(client: DiscourseClient):
+    return client._get('/notifications.json')['']
+
+
+def getReplyType(client: DiscourseClient):
+    return client._get('/notifications.json')['notifications'][0]['notification_type']
+
+
+def getPostReplyContent(client: DiscourseClient):
+    notification: dict = client._get('/notifications.json')[
+        'notifications'][0]
+    try:
+        post_number: int = int(notification['post_number'])
+    except TypeError:
+        return
+
+    topic_id: int = notification['topic_id']
+
+    return client.post(topic_id=topic_id, post_id=post_number)['post_stream']['posts'][len(client.post(topic_id=topic_id, post_id=post_number)['post_stream']['posts']) - 1]['cooked']
 
 
 class Lexobot:
@@ -20,15 +64,23 @@ class Lexobot:
             api_key=self.api_key)
 
         print("Connected!")
-        notifs: dict = getNotifications(client)
+
         while True:
-            if getNotifications(client)[0] != notifs[0]:
+            if not getNotifications(client)[0]['read'] and getReplyType(client) != 5:
                 print(htmlStripper(getPostReplyContent(client)))
 
-                client.create_post(
-                    "This is a dummy message! In order to further communicate with me, I need more practice!", getPostReplyNumber(client), getTopicReplyNumber(client))
-                notifs = getNotifications(client)
+                # client.create_post(
+                #     "This is a dummy message! In order to further communicate with me, I need more practice!", getPostReplyNumber(client), getTopicReplyNumber(client))
 
-            time.sleep(0.01)
+                print(getReplyType(client=client))
+                time.sleep(1)
+
+            # time.sleep(0.01)
     # client.create_post("Hello! I'm Lexobot. I'm still learning to wield the power of my own existence, so please bear with me!",
     #                    TESTING_CATEGORY, TESTING_THREAD)
+
+
+lexobot = Lexobot(host=HOST, api_key=API_KEY, username=USERNAME)
+
+if __name__ == "__main__":
+    print(lexobot())
